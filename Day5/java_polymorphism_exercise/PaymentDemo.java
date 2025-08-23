0444a63bce1c67e208e2d2148d886fa5da42a1d8
Santhosh, Amara (A.) <asanth16+ford@ford.com>
2025-08-23 15:46:28 +0530

    Added Assignments

---
+++ Day5/java_polymorphism_exercise/PaymentDemo.java
@@ -0,0 +1,47 @@
+
+public class PaymentDemo {
+    public static void main(String[] args) {
+        Payment paymentRef;
+
+        // Payment reference holds a CreditCardPayment object
+        paymentRef = new CreditCardPayment();
+        paymentRef.processPayment(500.0);
+
+        // Payment reference holds a UPIPayment object
+        paymentRef = new UPIPayment();
+        paymentRef.processPayment(300.0);
+
+        // Payment reference holds a CashPayment object
+        paymentRef = new CashPayment();
+        paymentRef.processPayment(200.0);
+
+    }
+}
+
+class Payment {
+    // Method intended to be overridden
+    public void processPayment(double amount) {
+        System.out.println("Processing generic payment of Rs. " + amount);
+    }
+}
+
+class CreditCardPayment extends Payment {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing credit card payment of Rs. " + amount);
+    }
+}
+
+class UPIPayment extends Payment {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing UPI payment of Rs. " + amount);
+    }
+}
+
+class CashPayment extends Payment {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing cash payment of Rs. " + amount);
+    }
+}
